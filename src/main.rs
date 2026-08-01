0760658bcacab4089b139ab29eda29c4ac62da00
Jackelyn <jackelyngironvillacinda@gmail.com>
2026-08-01 02:40:59 -0600

    Archivos iniciales, framebuffer y bmp

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use minifb::{Key, Window, WindowOptions};
+
+use std::time::Duration;
+
+mod framebuffer;
+
+use crate::framebuffer::Framebuffer;
+
+fn main() {
+    let window_width = 800;
+    let window_height = 600;
+    let framebuffer_width = 80;
+    let framebuffer_height = 60;
+
+    let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height);
+
+    let mut window = Window::new(
+        "Conway´s game of life",
+        window_width,
+        window_height,
+        WindowOptions::default(),
+    )
+    .unwrap();
+
+    framebuffer.set_background_color(0x000000);
+
+    let mut x = 0_i32;
+    let mut x2 = 1_i32;
+    let mut x3 = 2_i32;
+    let mut speed_x = 1_i32;
+
+    let mut y = 40_i32;
+    let mut speed_y = 1_i32;
+
+    let frame_delay = Duration::from_millis(16);
+
+    framebuffer.set_current_color(0xFFFFFF);
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        if x3 as usize >= framebuffer_width {
+            framebuffer.set_current_color(0xF17102);
+            speed_x = -1;
+        }
+        if x <= 0 {
+            framebuffer.set_current_color(0x00FF00);
+            speed_x = 1;
+        }
+
+        x += speed_x;
+        x2 += speed_x;
+        x3 += speed_x;
+
+        // Rebote vertical.
+        if y as usize >= framebuffer_height {
+            speed_y = -1;
+        }
+        if y <= 0 {
+            speed_y = 1;
+        }
+
+        y += speed_y;
+
+        framebuffer.clear();
+        framebuffer.point(x as usize, y as usize);
+        framebuffer.point(x2 as usize, y as usize);
+        framebuffer.point(x3 as usize, y as usize);
+
+
+        window
+            .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
+            .unwrap();
+        std::thread::sleep(frame_delay);
+    }
+}
